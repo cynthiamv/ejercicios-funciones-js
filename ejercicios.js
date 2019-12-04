c910b40988a94a0e130ab85e3163c6742461ed5e
cynthia.mvico <cynthia.mvico@gmail.com>
2019-12-04 11:48:31 -0300

    agregando ejercicios con return 3, 4 y 5

--- ejercicios.js
+++ ejercicios.js
@@ -54,7 +54,7 @@ comprobarPosicion(["milanesa", "fugazzeta rellena", "canelones"], "fugazzeta rel
 const calcularPromedio = (array) => {
     let sumaParcial = 0;
     for (let i = 0; i < array.length; i++) {
-        sumaParcial += array[i]    
+        sumaParcial += array[i]
     }
     console.log(sumaParcial / array.length)
 }
@@ -92,3 +92,52 @@ const obtenerSaludo = () => {
 }
 
 console.log(obtenerSaludo());
+
+
+//EJERCICIO 3
+//Crear la funcion obtenerResto(a, b) que retorne el resto de dividir a y b.
+
+const obtenerResto = (a, b) => {
+    return a % b
+};
+
+console.log(obtenerResto(5, 2));
+
+
+// 4. Crear la funcion esPar(num) que retorne true si el numero es par, y false si es impar.
+// Dentro de esa funcion debe utilizarse lo retornado por la funcion obtenerResto creada en 2.3.
+
+const esPar = num => {
+    if (obtenerResto(num, 2) === 0) {
+        return true
+    } else {
+        return false
+    }
+
+};
+
+console.log(esPar(5))
+// forma simplificada 
+// const esPar = num => obtenerResto(num, 2) === 0
+
+
+// EJERCICIO 5
+// Declarar la siguiente variable:
+// const listaDeNumeros = [ 43, 11, 18, 46, 44, 37, 42, 29, 9, 3, 37, 0, 40, 10, 38, 34, 25, 40, 4, 32 ];
+// Crear una funcion llamada encontrarImpares() que recorre el array recibido por parametros
+// y retorna un nuevo array compuesto solo por los numeros impares.
+// Para saber si un número es par debe utilizarse la funcion esPar() creada en 2.4.
+
+const listaDeNumeros = [43, 11, 18, 46, 44, 37, 42, 29, 9, 3, 37, 0, 40, 10, 38, 34, 25, 40, 4, 32];
+
+const encontrarImpares = array => {
+    let numerosImpares = []
+    for (let i = 0; i < array.length; i++) {
+        if (!esPar(array[i])) {
+            numerosImpares.push(array[i])
+        }
+    }
+    return numerosImpares
+};
+
+console.log(encontrarImpares(listaDeNumeros));
